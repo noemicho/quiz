c96c38c067fb8c0d6fe70a8b435dca95db5cf7b2
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-02-17 13:14:12 -0300

    Context React

--- my-quiz/src/components/HeaderHome.jsx
+++ my-quiz/src/components/HeaderHome.jsx
@@ -1,7 +1,12 @@
 import Quiz from '../img/quiz.png'
 import '../components/HeaderHome.css'
 
+import { useContext } from 'react'
+import { QuizContext } from '../context/Quiz'
+
 const HeaderHome = () => {
+  const quizState = useContext(QuizContext);
+  console.log(quizState)
   return (
     <div className="HeaderHome">
       <h3>Você conhece o básico sobre mim?</h3>
